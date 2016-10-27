c7c5bf3c4b467361ec98d4a34d046249dfab10cd
xaterz <ruofan.yu@gmail.com>
2016-10-27 19:02:29 +0800

    Added colour picker

--- recolor.py
+++ recolor.py
@@ -2,9 +2,50 @@ import numpy as np
 import cv2
 
 image = cv2.imread("test.jpg")
+greybar = np.zeros((60, image.shape[1], 3), np.uint8)
+greybar.fill(187)
+
+#row,col,cha = greybar.shape
+for r in range(7, 54):
+    for c in range(7, 54):
+        greybar[r,c] = (0,0,0)
+
+colorPicked = None
+changeColor = False
+imgShape = image.shape
+def getBGRAtMouse(event, x, y, flags, param):
+    global colorPicked
+    global changeColor
+    global imgShape
+    if event == cv2.EVENT_LBUTTONDOWN:
+        bgr = display[y,x]
+        print "({}\t{}):\t[{}\t{}\t  {}]".format(x, y, bgr[0], bgr[1], bgr[2])
+        if y < imgShape[0]:
+            colorPicked = bgr
+            changeColor = True
+
+while True:
+    if cv2.getWindowProperty('window-name', 0) < 0:
+        cv2.namedWindow('images')
+        cv2.setMouseCallback('images', getBGRAtMouse)
+        display = np.vstack([image, greybar])
+        
+    if changeColor:
+        for r in range(7, 54):
+            for c in range(7, 54):
+                greybar[r,c] = colorPicked
+        display = np.vstack([image, greybar])
+        changeColor = False
+        
+    cv2.imshow("images", display)
+    k = cv2.waitKey(10)
+    if k == 13:  # PRESS ENTER TO CONFIRM COLOUR
+        break
+cv2.destroyAllWindows()
 
 #lower = [17, 15, 100]
 #upper = [50, 56, 200]
+print "Colour picked:", colorPicked
 lower = [10, 10, 70]
 upper = [115, 100, 240]
 
@@ -14,6 +55,7 @@ upper = np.array(upper, dtype = "uint8")
 mask = cv2.inRange(image, lower, upper)
 extract = cv2.bitwise_and(image, image, mask = mask)
 
+'''
 display = np.hstack([image, extract])
 while True:    
     cv2.imshow("images", display)
@@ -21,11 +63,10 @@ while True:
     if k == 27 or cv2.getWindowProperty('window-name', 0) < 0:
         break
 cv2.destroyAllWindows()
+'''
 
 img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 img_h, img_s, img_v = cv2.split(img_hsv)
-ext_hsv = cv2.cvtColor(extract, cv2.COLOR_BGR2HSV)
-ext_h, ext_s, ext_v = cv2.split(ext_hsv)
 ext_b, ext_g, ext_r = cv2.split(extract)
 row,col = mask.shape
 
@@ -41,11 +82,6 @@ for r in range(row):
 
 img_hsv = cv2.merge((img_h, img_s, img_v))
 output = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
-
-def getBGRAtMouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        bgr = display[y,x]
-        print "({}\t{}):\t[{}\t{}\t  {}]".format(x, y, bgr[0], bgr[1], bgr[2])
 
 cv2.namedWindow('images')
 cv2.setMouseCallback('images', getBGRAtMouse)
